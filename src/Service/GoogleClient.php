d7b9a24af8c8b4eb2f315487e4d94ff99bc4bd84
Max Bachhuber <max.bachhuber@bahuma.io>
2022-02-05 11:58:04 +0100

    Add logging for access token

--- src/Service/GoogleClient.php
+++ src/Service/GoogleClient.php
@@ -2,29 +2,49 @@
 
 namespace App\Service;
 
+use Exception;
 use GuzzleHttp\Client;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use League\OAuth2\Client\Token\AccessToken;
+use Psr\Log\LoggerInterface;
 
 class GoogleClient
 {
 
+    protected KeyValueStore $keyValueStore;
+    protected ClientRegistry $clientRegistry;
+    protected LoggerInterface $logger;
+
     protected AccessToken $accessToken;
 
-    public function __construct(KeyValueStore $keyValueStore, ClientRegistry $clientRegistry)
+    public function __construct(KeyValueStore $keyValueStore, ClientRegistry $clientRegistry, LoggerInterface $logger)
     {
-        $tkn = $keyValueStore->get("google_access_token");
+        $this->keyValueStore = $keyValueStore;
+        $this->clientRegistry = $clientRegistry;
+        $this->logger = $logger;
+
+        $this->getAccessToken();
+    }
+
+    protected function getAccessToken()
+    {
+        $tkn = $this->keyValueStore->get("google_access_token");
+        $this->logger->debug('Access token: ' . $tkn);
         if ($tkn == null) {
-            throw new \Exception("Google Account is not connected. Please visit /connect/google!");
+            throw new Exception("Google Account is not connected. Please visit /connect/google!");
         }
 
         $this->accessToken = $tkn;
 
         if ($this->accessToken->hasExpired()) {
-            $refreshToken = $keyValueStore->get('google_refresh_token');
-            $oauthClient = $clientRegistry->getClient('google');
+            $this->logger->info('Access token for Google Client expired. Trying to refresh.');
+            $refreshToken = $this->keyValueStore->get('google_refresh_token');
+            $oauthClient = $this->clientRegistry->getClient('google');
             $newToken = $oauthClient->refreshAccessToken($refreshToken);
-            $keyValueStore->set('google_access_token', $newToken);
+            $this->logger->info('New accessToken from refresh', [
+                'access_token' => $newToken,
+            ]);
+            $this->keyValueStore->set('google_access_token', $newToken);
             $this->accessToken = $newToken;
         }
     }
